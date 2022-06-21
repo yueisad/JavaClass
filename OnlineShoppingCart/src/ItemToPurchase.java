6e4af565fc6221c2ef092335ad1e5b264e166fef
Alden Park <jackleekpop35@gmail.com>
2022-06-20 17:45:34 -0700

    changes

---
+++ OnlineShoppingCart/src/ItemToPurchase.java
@@ -0,0 +1,39 @@
+public class ItemToPurchase {
+
+    //private fields
+    private String itemName;
+    private int itemPrice;
+    private int itemQuantity; 
+
+    //default constructor 
+    public ItemToPurchase(){
+        this.itemName = "none";
+        this.itemPrice = 0; 
+        this.itemQuantity = 0; 
+    }
+
+    public void setName(String itemName) {
+        this.itemName = itemName;
+    }
+    public String getName() {
+        return itemName;
+    }
+
+
+    public void setPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+    public int getPrice(){
+        return itemPrice;
+    }
+
+
+    public void setQuantity(int itemQuantity){
+        this.itemQuantity = itemQuantity;
+    }
+    public int getQuantity() {
+        return itemQuantity;
+    }
+
+    
+}
---
+++ OnlineShoppingCart/src/ShoppingCartPrinter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ShoppingCartPrinter {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+
+    }
+
+}
