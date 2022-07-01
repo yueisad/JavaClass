7a86dcf32558d12dda63dc8611512cc67adab8ab
Alden Park <jackleekpop35@gmail.com>
2022-06-30 17:52:03 -0700

    exercise

---
+++ Instrument thing/src/InstrumentInformation.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+//TO DO
+// set the user input into its class variables
+// display the instrument type, manufacturer, year built, cost, number of strings,
+// and number of frets each on a separate line
+
+public class InstrumentInformation {
+	
+   public static void main(String[] args) {
+      
+
+      getInstrumentInfo();
+
+   } //end main
+   
+   public static void getInstrumentInfo() {
+	   
+	   //variables
+	   String instrumentType, manufacturerName, stringInstrumentName, stringManufacturer;
+	      int yearBuilt, cost, stringYearBuilt, stringCost, numStrings, numFrets;
+	   
+	   Scanner scnr = new Scanner(System.in);
+	   
+	   
+	   System.out.print("Enter String Instrument Type: ");
+	   stringInstrumentName = scnr.nextLine();
+	   System.out.println("Enter Manufacturer Name: ");
+	   stringManufacturer = scnr.nextLine();
+	   System.out.println("Enter Year Built: ");
+	   stringYearBuilt = scnr.nextInt();
+	   System.out.println("Enter cost: $");
+	   stringCost = scnr.nextInt();
+	   System.out.println("Enter the number of strings: ");
+	   numStrings = scnr.nextInt();
+	   System.out.println("Enter the number of frets: ");
+	   numFrets = scnr.nextInt();
+	   
+	   StringInstrument myStringInstrument = new StringInstrument(stringInstrumentName, stringManufacturer, stringYearBuilt, stringCost, numStrings, numFrets);
+
+
+	   myStringInstrument.printInfo();
+	   
+   } //end getInstrumentInfo
+}
